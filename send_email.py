9c9e85dbe9137e5b16c8cb85923fb7d9524c412c
Ruslan <suleimanovrn@gmail.com>
2025-07-03 06:48:25 +0300

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -7,31 +7,15 @@ load_dotenv()
 email_login =os.environ.get('LOGIN')
 email_password =os.environ.get('TOKEN')
 
-from_address = "RuslanSuleimonov@yandex.ru"
-to_address = "RuslanSuleimonov@yandex.ru"
+from_address = "@yandex.ru"
+to_address = "@yandex.ru"
 subject = "Приглашение!"
 
-template = """Привет, {friend_name}! {my_name} приглашает тебя на сайт {website}!
-
-{website} — это новая версия онлайн-курса по программированию. 
-Изучаем Python и не только. Решаем задачи. Получаем ревью от преподавателя. 
-
-Как будет проходить ваше обучение на {website}? 
-
-→ Попрактикуешься на реальных кейсах. 
-Задачи от тимлидов со стажем от 10 лет в программировании.
-→ Будешь учиться без стресса и бессонных ночей. 
-Задачи не «сгорят» и не уйдут к другому. Занимайся в удобное время и ровно столько, сколько можешь.
-→ Подготовишь крепкое резюме.
-Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
-
-Регистрируйся → {website}  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.
-"""
+template = """Привет, {friend_name}! {my_name} приглашает тебя на сайт {website}!"""
 
 friend_name = "Темирлан"
 my_name = "Руслан"
-website = "https://dvmn.org/profession-ref-program/suleimanovrn/TTabC/"
+website = ""
 
 invitation = template.format(friend_name=friend_name, my_name=my_name, website=website)
 
